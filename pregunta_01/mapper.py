6b2ae7218a14e9ec9775c6cd2fd455671731b87c
yavelasquezo <69699917+yavelasquezo@users.noreply.github.com>
2022-06-10 15:42:12 -0500

    Update mapper.py

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,15 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+file=open("credit.csv", "r").readlines()
+
+file=[z.replace("\n","")for z in file]
+file=[z.split(",") for z in file]
+lista=[]
+
+#Map
+for i in file:
+  lista.append(i[2])
+lista.sort(reverse=False)
+result=lista
