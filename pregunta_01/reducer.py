a13aa77b8d9fcd6ac05a19c1317052cc255cf674
yavelasquezo <69699917+yavelasquezo@users.noreply.github.com>
2022-06-10 15:43:09 -0500

    Update reducer.py

--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -1,3 +1,15 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+
+from functools import reduce
+def make_counts(acc, nxt):
+    acc[nxt] = acc.get(nxt, 0) + 1
+    return acc
+
+
+val=str(reduce(
+    make_counts,
+    result,
+    {},
+))
