1ce253037f6093f3692fb2bf1682266fb9360d00
yavelasquezo <69699917+yavelasquezo@users.noreply.github.com>
2022-06-15 07:49:38 -0500

    Update mapper.py

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -1,3 +1,16 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    for row in sys.stdin:
+        # elimino los espacios en blanco y divido por los 3 espacios que hay entre columnas
+        dividir = row.strip().split("   ")
+        columnaUno = dividir[1]
+        fechaDividida = columnaUno.split("-")
+        mes = fechaDividida[1]
+  
+    
+        sys.stdout.write(f"{mes}\t1\n")
