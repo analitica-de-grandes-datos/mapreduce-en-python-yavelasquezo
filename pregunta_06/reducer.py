dc4bb747ff6d4d8777a312383422fdc526c58ec7
yavelasquezo <69699917+yavelasquezo@users.noreply.github.com>
2022-06-15 07:51:35 -0500

    Update reducer.py

--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -1,3 +1,56 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+from __future__ import division
+import sys
+
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+if __name__ == '__main__':
+
+    curkey = None
+    max = 0
+    min = 0
+    
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
+    for line in sys.stdin:
+        
+        key, val = line.split("\t")
+        val = float(val)
+              
+        if key == curkey:
+            #
+            # No se ha cambiado de clave. Aca se acumulan los valores para la misma
+            # clave.
+            #
+            if val > max:
+                max = val
+            else:
+                val = max
+
+            if val < min:
+                min = val
+            else:
+                val = min
+
+            #total += val
+        else:
+            #
+            # Se cambio de clave. Se reinicia el acumulador.
+            #
+            if curkey is not None:
+                #
+                # una vez se han reducido todos los elementos
+                # con la misma clave se imprime el resultado en
+                # el flujo de salida
+                #
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, max, min))
+
+            curkey = key
+            max = val
+            min = val
+
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, max, min))
