94f5f2e011feb1c5e8ce6d4b55c96a30371894a8
yavelasquezo <69699917+yavelasquezo@users.noreply.github.com>
2022-06-15 07:45:26 -0500

    Update mapper.py

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,23 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+
+
+if __name__ == "__main__":
+
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
+
+    for row in sys.stdin:
+        # limpio los espacios en blanco al final
+        limpiarLinea = row.strip()
+        
+        #divido por por coma y obtengo las dos columnas que me interan       
+        letra, numero = limpiarLinea.split(",") 
+
+        # escribe al flujo estandar de salida
+        sys.stdout.write(f"{letra}\t{numero}\n")
