4c6acb25dfe5a2891f8530f120c642082b3404f4
yavelasquezo <69699917+yavelasquezo@users.noreply.github.com>
2022-06-14 20:01:31 -0500

    terminado

--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -2,19 +2,26 @@
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
 
-from signal import valid_signals
 import sys
-if __name__ =="__main__":
+if __name__ =='__main__':
+
     curkey=None
     total=0
+
     for line in sys.stdin:
+
         key,val=line.split('\t')
         val=int(val)
+
         if key == curkey:
+
             total+=val
+
         else:
             if curkey is not None:
+
                 sys.stdout.write("{}\t{}\n".format(curkey,total))
+            
             curkey=key
             total=val
-        sys.stdout.write("{}\t{}\n".format(curkey,total))
+    sys.stdout.write("{}\t{}\n".format(curkey,total))
