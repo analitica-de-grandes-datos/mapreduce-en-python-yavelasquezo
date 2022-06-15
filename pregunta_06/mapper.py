20d45884a6f552b68d35e219d3e2cdf8cd960d76
yavelasquezo <69699917+yavelasquezo@users.noreply.github.com>
2022-06-15 07:51:06 -0500

    Update mapper.py

--- pregunta_06/mapper.py
+++ pregunta_06/mapper.py
@@ -1,3 +1,15 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    for row in sys.stdin:
+        # elimino los espacios en blanco y divido por los 3 espacios que hay entre columnas
+        dividir = row.strip().split("   ")
+        columnaCero = dividir[0]
+        columnaDos = dividir[2]
+  
+    
+        sys.stdout.write(f"{columnaCero}\t{columnaDos}\n")
