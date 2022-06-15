5c3d0dfc95173b0e09d317a7665aace010b7a446
yavelasquezo <69699917+yavelasquezo@users.noreply.github.com>
2022-06-15 07:47:03 -0500

    Update mapper.py

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -4,8 +4,16 @@
 import sys
 
 if __name__ == "__main__":
-  for line in sys.stdin:
 
-    lista=line.split(',')
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
+   
+    #for line in sys.stdin:
+    for row in sys.stdin:
+        # elimino los espacios en blanco y divido por los 3 espacios que hay entre columnas
+        dividir = row.strip().split("   ")
+        columnaUno = dividir[0]
     
-    sys.stdout.write("{}\t1\n".format(lista[0]))
+        sys.stdout.write(f"{columnaUno}\t1\n")
