8f25e5c99ba1feaf60e3a4d7026ecd9f9a2e6969
yavelasquezo <69699917+yavelasquezo@users.noreply.github.com>
2022-06-15 07:47:34 -0500

    Update reducer.py

--- pregunta_04/reducer.py
+++ pregunta_04/reducer.py
@@ -2,25 +2,39 @@
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
 import sys
-if __name__ =='__main__':
 
-    curkey=None
-    total=0
+if __name__ == '__main__':
 
+    curkey = None
+    total = 0
+
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
     for line in sys.stdin:
 
-        key,val=line.split('\t')
-        val=int(val)
+        key, val = line.split("\t")
+        val = int(val)
 
         if key == curkey:
-
-            total+=val
-
+            #
+            # No se ha cambiado de clave. Aca se acumulan los valores para la misma
+            # clave.
+            #
+            total += val
         else:
+            #
+            # Se cambio de clave. Se reinicia el acumulador.
+            #
             if curkey is not None:
+                #
+                # una vez se han reducido todos los elementos
+                # con la misma clave se imprime el resultado en
+                # el flujo de salida
+                #
+                sys.stdout.write("{},{}\n".format(curkey, total))
 
-                sys.stdout.write("{}\t{}\n".format(curkey,total))
-            
-            curkey=key
-            total=val
-    sys.stdout.write("{}\t{}\n".format(curkey,total))
+            curkey = key
+            total = val
+
+    sys.stdout.write("{},{}\n".format(curkey, total))
