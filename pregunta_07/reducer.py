872275aa7f1eb4a3cce7741e62f97c08a7a2aac0
yavelasquezo <69699917+yavelasquezo@users.noreply.github.com>
2022-06-15 07:53:09 -0500

    Update reducer.py

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,26 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import operator
+import sys
+
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+if __name__ == '__main__':
+
+    lista = []
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
+    for line in sys.stdin: 
+        key, fecha, valor = line.split("\t")
+        valor = int(valor)
+        tupla = key, fecha, valor
+        lista.append(tupla)
+          
+    sortedDict = sorted(lista, key=operator.itemgetter(0, 2))
+    
+    for linea in sortedDict:
+        
+        sys.stdout.write("{}   {}   {}\n".format(linea[0], linea[1], linea[2]))
