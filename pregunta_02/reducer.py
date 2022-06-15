93741d3bbdf3f2eade90c317b1e7edaf9a34d276
yavelasquezo <69699917+yavelasquezo@users.noreply.github.com>
2022-06-14 20:10:33 -0500

    Update reducer.py

--- pregunta_02/reducer.py
+++ pregunta_02/reducer.py
@@ -1,3 +1,28 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+if __name__ =='__main__':
+
+    curkey=None
+    maximo=[]
+
+    for line in sys.stdin:
+
+        key,val=line.split('\t')
+        val=int(val)
+
+        if key == curkey:
+
+            maximo.append(val)
+
+        else:
+            if curkey is not None:
+
+                sys.stdout.write("{}\t{}\n".format(curkey,total))
+            
+            curkey=key
+            maximo.append[val]
+        total=max(maximo)
+        maximo=0
+    sys.stdout.write("{}\t{}\n".format(curkey,total))
